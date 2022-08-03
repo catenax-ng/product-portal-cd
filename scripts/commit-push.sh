ff9a94693f36bc0fff874b9d78cf25c52a62c2b4
Evelyn Gurschler <evelyn.gurschler@bmw.de>
2022-08-03 16:18:46 +0200

    Update script

--- scripts/commit-push.sh
+++ scripts/commit-push.sh
@@ -1,3 +1,7 @@
+git config user.name github-actions
+git config user.email github-actions@github.com
+git add chart/portal-backend/values-dev.yaml
+git commit -m "Add new image for backend service"
 success=1
 attempts=0
 until [ $success == 0 ] || [ $attempts == 10 ]
